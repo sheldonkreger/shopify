f779bf0cb7113b8f265890ed29afcab4ee1f81e0
Boris Gnezdilov <sysashi.boris@gmail.com>
2017-12-11 06:57:32 +0800

    Rename @default_methods to @default_verbs
    Remove unnecessary function

--- lib/shopify/admin_api/verbs.ex
+++ lib/shopify/admin_api/verbs.ex
@@ -1,35 +1,35 @@
 defmodule Shopify.AdminAPI.Resource.Verbs do
   alias Shopify.AdminAPI.Resource
 
-  @default_methods [:all, :get, :create, :update, :delete]
-  defmacro generate(opts) do
-    name = Keyword.fetch!(opts, :name)
-    collection = Keyword.get(opts, :collection)
-    verbs = Keyword.get(opts, :only, @default_methods)
+  @moduledoc false
 
-    Module.put_attribute(__CALLER__.module, :resource, Inflex.pluralize(name))
+  @default_verbs [:all, :get, :create, :update, :delete]
+  defmacro generate(opts) do
+    resource = Keyword.fetch!(opts, :name)
+    collection = Keyword.get(opts, :collection)
+    verbs = Keyword.get(opts, :only, @default_verbs)
+
+    Module.put_attribute(__CALLER__.module, :resource, Inflex.pluralize(resource))
     Module.put_attribute(__CALLER__.module, :collection, Inflex.pluralize(collection))
 
-    unless valid_verbs?(verbs, @default_methods) do
+    unless valid_verbs?(verbs, @default_verbs) do
       raise ArgumentError, "option 'only' should be a list and supports " <>
-                           "following functions #{inspect @default_methods}, got #{inspect verbs}"
+                           "following functions #{inspect @default_verbs}, got #{inspect verbs}"
     end
 
-    Enum.map(verbs, &(apply(Resource.Verbs, :verb, [&1, name, collection])))
-  end
-
-  def verb(verb, resource, collection) do
-    {verb_args, method_args} = args(verb, resource, collection)
-    method = verb_method(verb)
-    name = name(verb, resource)
-    params_to = params_to(verb)
-    query_or_body = Macro.var(params_to, __MODULE__)
-    quote do
-      def unquote(verb)(unquote_splicing(verb_args), unquote(query_or_body) \\ %{}) do
-        apply Resource, unquote(method), [
-          [unquote_splicing(method_args)],
-          [{unquote(params_to), unquote(query_or_body)}, {:name, unquote(name)}]
-        ]
+    for verb <- verbs do
+      {verb_args, method_args} = args(verb, resource, collection)
+      method = verb_method(verb)
+      name = name(verb, resource)
+      params_to = params_to(verb)
+      query_or_body = Macro.var(params_to, __MODULE__)
+      quote do
+        def unquote(verb)(unquote_splicing(verb_args), unquote(query_or_body) \\ %{}) do
+          apply Resource, unquote(method), [
+            [unquote_splicing(method_args)],
+            [{unquote(params_to), unquote(query_or_body)}, {:name, unquote(name)}]
+          ]
+        end
       end
     end
   end
@@ -63,15 +63,15 @@ defmodule Shopify.AdminAPI.Resource.Verbs do
     is_list(provided) and MapSet.subset?(MapSet.new(provided), MapSet.new(default))
   end
 
-  defp name(:all, resource), do: Inflex.pluralize(resource)
-  defp name(_, resource),    do: resource
-
   defp verb_method(:all),    do: :get
   defp verb_method(:get),    do: :get
   defp verb_method(:create), do: :post
   defp verb_method(:update), do: :put
   defp verb_method(:delete), do: :delete
 
-  defp params_to(verb) when verb in ~w(create update delete)a,  do: :body
+  defp name(:all, resource), do: Inflex.pluralize(resource)
+  defp name(_, resource),    do: resource
+
+  defp params_to(verb) when verb in ~w(create update delete)a, do: :body
   defp params_to(verb) when verb in ~w(all get)a, do: :query
 end
