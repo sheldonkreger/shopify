788142df1a773af4eec1a75a5e819863e5166311
Boris Gnezdilov <sysashi.boris@gmail.com>
2018-01-21 03:51:04 +0800

    Wrap resource body in its name before encoding

--- lib/shopify/admin_api/resource.ex
+++ lib/shopify/admin_api/resource.ex
@@ -40,7 +40,7 @@ defmodule Shopify.AdminAPI.Resource do
   def prepare(resource = %__MODULE__{}) do
     Map.new(method: validate_method(resource.method),
             path: to_path(resource.path, resource.query),
-            body: encode_body(resource.body))
+            body: encode_body(resource.body, resource.name))
   end
 
   defp to_path(path, query) when is_binary(path),
@@ -48,22 +48,23 @@ defmodule Shopify.AdminAPI.Resource do
   defp to_path(path, query) when is_list(path),
     do: make_path(Enum.join(path, "/"), query)
 
-  defp encode_body(body) when is_map(body) or is_list(body), do: Poison.encode!(body)
-  defp encode_body(body) when is_binary(body), do: body
-  defp encode_body(nil), do: ""
+  defp encode_body(body, name) when is_map(body) or is_list(body),
+    do: Poison.encode!(%{name => Map.new(body)})
+  defp encode_body(body, _) when is_binary(body), do: body
+  defp encode_body(_, _), do: ""
 
   @methods ~w(get post delete put patch)a
   defp validate_method(method) when method in @methods, do: method
   defp validate_method(method),
     do: raise ArgumentError, "Resource method must be one of the #{@methods}, got: #{inspect method}"
 
-  defp to_query(q) when is_list(q) or is_map(q), do: URI.encode_query(q)
-  defp to_query(q) when is_binary(q), do: q
-  defp to_query(_), do: nil
-
   defp make_path(path, query) do
     %URI{path: path <> ".json", query: to_query(query)}
     |> to_string()
     |> String.trim_trailing("?")
   end
+
+  defp to_query(q) when is_list(q) or is_map(q), do: URI.encode_query(q)
+  defp to_query(q) when is_binary(q), do: q
+  defp to_query(_), do: nil
 end
